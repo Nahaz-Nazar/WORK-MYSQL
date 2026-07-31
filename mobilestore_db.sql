94c39661efc8a47ed07bfdbb7ca5fd370414d577
nahaz <nahaznazar73@gmail.com>
2026-07-31 15:57:28 +0530

    Create mobilestore database and define mobiles table with sample data and queries

---
+++ mobilestore_db.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE mobilestore_db;
+USE mobilestore_db;
+
+
+CREATE TABLE mobiles (
+    id INT PRIMARY KEY,
+    brand VARCHAR(50),
+    model VARCHAR(50),
+    price INT,
+    stock INT
+);
+
+INSERT INTO mobiles (id, brand, model, price, stock)
+VALUES
+(1, 'Samsung', 'Galaxy M14', 12000, 30),
+(2, 'Redmi', 'Note 12', 15000, 25),
+(3, 'Realme', 'Narzo 50', 13000, 20),
+(4, 'Samsung', 'Galaxy A23', 18000, 10);
+
+SELECT *
+FROM mobiles
+WHERE price > 13000 OR stock < 15;
+
+UPDATE mobiles
+SET stock = stock + 5,
+    price = 12500
+WHERE model = 'Narzo 50';
+
+DELETE FROM mobiles
+WHERE id = 2;
+
+SELECT MIN(price) AS Lowest_Price,
+       MAX(price) AS Highest_Price
+FROM mobiles;
+
+SELECT SUM(stock) AS Total_Stock
+FROM mobiles;
+
+SELECT *
+FROM mobiles
+ORDER BY price DESC
+LIMIT 2;
